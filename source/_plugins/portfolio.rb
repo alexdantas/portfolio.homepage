59f243fe52dc2060ef3882ea585e47baa7870a10
Alexandre Dantas <eu@alexdantas.net>
2014-10-13 20:32:24 -0300

    Created the pages "websites" and "graphical-arts"

--- source/_plugins/portfolio.rb
+++ source/_plugins/portfolio.rb
@@ -31,7 +31,71 @@ module Jekyll
           name   = File.basename(path, '.yml')
           config = YAML.load(File.read(path))
 
-          projects[name] = config if config['published']
+          if config['published'] and config['featured']
+            projects[name] = config
+          end
+        end
+      end
+    end
+  end
+
+  # Index for the projects under the "websites"
+  # category.
+  class PortfolioWebsitesIndex < Page
+    def initialize(site, base, dir)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = "websites.html"
+
+      @layouts_dir  = File.join(base, '_layouts')
+      @projects_dir = File.join(base, '_projects')
+
+      self.process(@name)
+      self.read_yaml(@layouts_dir, 'portfolio-websites.html')
+      self.data['projects'] = self.get_projects(site)
+    end
+
+    def get_projects(site)
+      {}.tap do |projects|
+        Dir["#{@projects_dir}/*.yml"].each do |path|
+          name   = File.basename(path, '.yml')
+          config = YAML.load(File.read(path))
+
+          if config['published'] and config['type'] == 'website'
+            projects[name] = config
+          end
+        end
+      end
+    end
+  end
+
+  # Index for the projects under the "websites"
+  # category.
+  class PortfolioGraphicalArtsIndex < Page
+    def initialize(site, base, dir)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = "arts.html"
+
+      @layouts_dir  = File.join(base, '_layouts')
+      @projects_dir = File.join(base, '_projects')
+
+      self.process(@name)
+      self.read_yaml(@layouts_dir, 'portfolio-graphical-arts.html')
+      self.data['projects'] = self.get_projects(site)
+    end
+
+    def get_projects(site)
+      {}.tap do |projects|
+        Dir["#{@projects_dir}/*.yml"].each do |path|
+          name   = File.basename(path, '.yml')
+          config = YAML.load(File.read(path))
+
+          if config['published'] and config['type'] == 'art'
+            projects[name] = config
+          end
         end
       end
     end
@@ -107,6 +171,20 @@ module Jekyll
 
     def write_portfolio_index(site)
       portfolio = PortfolioIndex.new(site, site.source, "")
+      portfolio.render(site.layouts, site.site_payload)
+      portfolio.write(site.dest)
+
+      site.pages << portfolio
+      site.static_files << portfolio
+
+      portfolio = PortfolioWebsitesIndex.new(site, site.source, "")
+      portfolio.render(site.layouts, site.site_payload)
+      portfolio.write(site.dest)
+
+      site.pages << portfolio
+      site.static_files << portfolio
+
+      portfolio = PortfolioGraphicalArtsIndex.new(site, site.source, "")
       portfolio.render(site.layouts, site.site_payload)
       portfolio.write(site.dest)
 
